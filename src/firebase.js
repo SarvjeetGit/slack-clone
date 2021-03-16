b3ce0507bfbe96f394c82d2a4035e210dc914709
SarvjeetGit <sarvjeetrocks@gmail.com>
2021-03-17 00:05:37 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyATqdn6R6Jch5VDSCkRyGdVq2jwhPD_YYo',
+    authDomain: 'slack-clone-ebb84.firebaseapp.com',
+    projectId: 'slack-clone-ebb84',
+    storageBucket: 'slack-clone-ebb84.appspot.com',
+    messagingSenderId: '612437412819',
+    appId: '1:612437412819:web:abf6ea57f40fa9fc04fd26',
+    measurementId: 'G-FBB6RD8N83',
+};
